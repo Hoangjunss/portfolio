a3010c3f905c126e3e22570e6a470b551d4bfed3
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-07-26 10:48:11 +0700

    khanh

---
+++ src/main/java/com/baconbao/portfolio/model/Profile.java
@@ -0,0 +1,26 @@
+package com.baconbao.portfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profile")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    @Id
+    private Integer id;
+    private String onjective;
+    private String education;
+    private String workExperience;
+    private String skills;
+    private String address;
+    private String contactInfo;
+}
---
+++ src/main/java/com/baconbao/portfolio/model/Project.java
@@ -0,0 +1,25 @@
+package com.baconbao.portfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+    @Id
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private boolean status;
+}
