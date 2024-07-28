865b23b5932d9bc5ed1d0c352750d47999867d86
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-07-28 11:38:26 +0700

    khanh

--- src/main/java/com/baconbao/portfolio/repository/NotificationRepository.java
+++ src/main/java/com/baconbao/portfolio/repository/NotificationRepository.java
@@ -4,6 +4,7 @@ import com.baconbao.portfolio.dto.NotificationDTO;
 import com.baconbao.portfolio.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,6 @@ import java.util.Optional;
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     Optional<Notification> findById(Integer integer);
     List<Notification> findByUserId(Integer idUser);
+    @Query("SELECT n FROM Notification n WHERE n.userSend.id = :userId")
+    List<NotificationDTO> getNotificationByUser(@Param("userId") Integer userId);
 }
